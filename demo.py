76bae47a0778d13b634c05f18c705cf9f81f2929
Masud-Shah <masudshah220@gmail.com>
2021-12-25 21:51:18 +0300

    demo.py fixed the bug

--- demo.py
+++ demo.py
@@ -5,16 +5,19 @@ while True:
     my_num = input("Number 1 to 10 : ")
     g_num = random.randint(1,10)
     if g_num == int(my_num):
-        print(f" {count} times lost")
+        print(f" After {count} times lost")
         sleep(1)
         print("Congratulation , finally You Win !!")
         break
     else:
         print(f"{my_num} that a Wrong Number")
+        
         sleep(1)
-        print("Try Again , May Be Your Will be Win Next Try")
+        print(".")
+        sleep(1)
+        print("...")
+        sleep(1)
+        print()
+        print("Try Again , May Be You Win Next Try")
         count += 1
         continue
-        
-        
-              
